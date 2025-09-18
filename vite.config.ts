176821b54c605c51d66ad6f9b12b9477a1b9e9bf
Yassen <yassenexc@gmail.com>
2025-09-18 12:16:41 +0300

    ASD
    ASD

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/portfolio/', // Set base to your repository name for GitHub Pages
   server: {
     host: "::",
     port: 8080,
